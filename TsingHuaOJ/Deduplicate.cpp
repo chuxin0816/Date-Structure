23e01604493cc1d31c4cee80139938013f29288c
chuxin <2670957513@qq.com>
2023-02-27 16:58:22 +0800

    Deduplicate 成功构建

---
+++ TsingHuaOJ/Deduplicate.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#define maxn 600000
+using namespace std;
+int hashing(const string& a) {
+  int hashval = 0;
+  for (auto e : a) hashval = (e + hashval * 31) % maxn;
+  return hashval;
+}
+int list[maxn];
+string ans[maxn];
+int main() {
+  int n;
+  string str;
+  cin >> n;
+  while (n--) {
+    cin >> str;
+    int pos = hashing(str);
+    while (list[pos] && str != ans[pos]) pos = (pos + 1) % maxn;
+    ans[pos] = str;
+    if (list[pos] == 1) cout << str << endl;
+    list[pos]++;
+  }
+  return 0;
+}
